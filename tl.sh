28a8f7085ae405f4663d70c8daf6657094b0a98e
Benjamin Brumbaugh <benbrumbaugh@gmail.com>
2024-04-24 19:00:22 -0700

    Fixed the command, needed a space

--- tl.sh
+++ tl.sh
@@ -1,2 +1,2 @@
-curl -s[URL]|shasum|read h;awk -v a="${h:0:2}" -v b="${h:3:2}" 'NR==a||NR==b{print}' /usr/share/dict/words|grep -q 'word1-word2'&&sh
+curl -s [URL] | shasum | read h; awk -v a="$(printf "%d" "0x${h:0:2}")" -v b="$(printf "%d" "0x${h:3:2}")" 'NR==a||NR==b{print}' /usr/share/dict/words | grep -q 'word1-word2' && sh
 
