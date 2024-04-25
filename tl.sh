2385a4f013480866d6decc0e5a7eb8e1e73e7737
Benjamin Brumbaugh <benbrumbaugh@gmail.com>
2024-04-24 18:09:08 -0700

    Reduced character count from 216 to 210

--- tl.sh
+++ tl.sh
@@ -1,2 +1,2 @@
-curl -sS [URL] | tee >(shasum -a 256 | cut -f1 -d' ' | fold -w2 | awk '{printf "%d ", "0x"$1}' | xargs -I{} awk 'NR=={}' /usr/share/dict/words | tr '\n' '-' | xargs -I{} test '{}' = 'word1 word2' && sh) >(cat)
+curl -sS [URL] | tee >(shasum -a 256 | read h _; echo ${h:0:2}${h:2:2} | xargs -I{} sh -c "awk 'NR==0x{}' /usr/share/dict/words" | tr '\n' '-' | xargs -I{} test '{}' = 'word1 word2' && sh) >(cat)
 
