d75f1385637b7c1590b0bf1aec2cc19c729acc56
Benjamin Brumbaugh <benbrumbaugh@gmail.com>
2024-04-25 12:31:09 -0700

    /usr/share/dict/words is not the same across UNIX systems. Furthermore, there looks to be no good candidate to replace it. So instead, we can use the first few characters of a full-length hash until we get the number of bits of entropy we need. 32 is probably sufficient for this context, and SHA-1 should be fine for the hash

--- tl.sh
+++ tl.sh
@@ -1,2 +1,1 @@
-curl -s [URL] | shasum | read h; awk -v a="$(printf "%d" "0x${h:0:2}")" -v b="$(printf "%d" "0x${h:3:2}")" 'NR==a||NR==b{print}' /usr/share/dict/words | grep -q 'word1-word2' && sh
-
+grep -q '^expected_hash_prefix' <<<$(curl -s[URL]|shasum|cut -d' ' -f1) && sh
