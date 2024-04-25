75829af643f1dc265ea51b19c76b9e4f01318c04
Benjamin Brumbaugh <benbrumbaugh@gmail.com>
2024-04-24 18:07:03 -0700

    Reduced the character count from 245 to 236 and made it a cleaner approach

--- tl.sh
+++ tl.sh
@@ -1,1 +1,1 @@
-curl -s https://raw.githubusercontent.com/manpoozle/TrustLaunch/main/tl.sh | shasum -a 256 -c <(echo "known_good_hash  -") && sh
+curl -s [URL] | tee >(shasum -a 256 | cut -d' ' -f1 | fold -w2 | awk '{printf "%d ", "0x"$1}' | xargs -I{} sh -c "awk 'NR=={}' /usr/share/dict/words" | tr '\n' '-' | xargs -I{} sh -c "test '{}' = 'word1 word2' && exec sh") >(cat)
