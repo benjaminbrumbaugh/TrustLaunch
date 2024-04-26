7c24e9fdea89c5811be08698cde9be7c3ea86144
Benjamin Brumbaugh <benbrumbaugh@gmail.com>
2024-04-25 18:30:46 -0700

    Updated the script with the hash from the new hashing method

--- tl.sh
+++ tl.sh
@@ -1,1 +1,1 @@
-grep -q '^403e2020efd7c89668a95a5c8d4cbfb2c5ed0399' <<< $(curl -s https://raw.githubusercontent.com/Manpoozle/TrustLaunch/main/tl.sh | shasum | cut -d ' ' -f1) && sh
+grep -q '^78fb38f212fa49029aff24c669a39648d9b4e68b' <<< $(curl -s https://raw.githubusercontent.com/Manpoozle/TrustLaunch/main/tl.sh | shasum | cut -d ' ' -f1) && sh
