f1a1a90e5d20e548622925e2adc1b81939507f33
Benjamin Brumbaugh <benbrumbaugh@gmail.com>
2024-04-29 12:30:47 -0700

    In the previous compact version, I had lost the piping of the script contents into the shell. This reintroduces that while trying to keep compact and clean.

--- trust_launch_template.sh
+++ trust_launch_template.sh
@@ -1,1 +1,1 @@
-grep -q '^{{EXPECTED_HASH_PREFIX}}' <<< $(curl -s {{URL}} | shasum | cut -d ' ' -f1) && sh
+s=$(curl -s {{URL}}); shasum <<< "$s" | cut -d' ' -f1 | grep -q '^{{EXPECTED_HASH_PREFIX}}' && sh <<< "$s"
