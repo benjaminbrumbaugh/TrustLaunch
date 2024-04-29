38290cc06d811554ef1fcdac11ffc7006f3e46ac
Benjamin Brumbaugh <benbrumbaugh@gmail.com>
2024-04-29 13:11:36 -0700

    Updated the tl.sh with the new template

--- tl.sh
+++ tl.sh
@@ -1,1 +1,1 @@
-s=$(curl -s https://raw.githubusercontent.com/manpoozle/trustlaunch/main/ts.sh); shasum <<< "$s" | cut -d' ' -f1 | grep -q '^78fb38f212fa49029aff24c669a39648d9b4e68b' && sh <<< "$s" || return 1
+s=$(curl -s https://raw.githubusercontent.com/manpoozle/trustlaunch/main/ts.sh); shasum <<< "$s" | cut -d' ' -f1 | grep -q '^78fb38f212fa49029aff24c669a39648d9b4e68b' && sh <<< "$s" || { echo "Stale launch script. Stopping." >&2; false; }
