7d12b70a216d433c6c357050e4745e4f89fd70d8
Benjamin Brumbaugh <benbrumbaugh@gmail.com>
2024-04-29 12:38:13 -0700

    Updated the ts.sh script to the latest version of the template

--- tl.sh
+++ tl.sh
@@ -1,1 +1,1 @@
-grep -q '^78fb38f212fa49029aff24c669a39648d9b4e68b' <<< $(curl -s https://raw.githubusercontent.com/Manpoozle/TrustLaunch/main/tl.sh | shasum | cut -d ' ' -f1) && sh
+s=$(curl -s https://raw.githubusercontent.com/manpoozle/trustlaunch/main/ts.sh); shasum <<< "$s" | cut -d' ' -f1 | grep -q '^78fb38f212fa49029aff24c669a39648d9b4e68b' && sh <<< "$s" || return 1
