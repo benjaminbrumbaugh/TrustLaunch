2ada93d381c333e07a7c3a4f0010ebe0bebe93de
Benjamin Brumbaugh <benbrumbaugh@gmail.com>
2024-05-24 12:36:35 -0700

    Made so the update-hash script pulls the remote script only once Updated it to pull the test script

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 # config.sh
 # URLs for the script and the one-liner template
-export script_url="https://raw.githubusercontent.com/Manpoozle/TrustLaunch/main/tl.sh"
-export one_liner_template_url="https://raw.githubusercontent.com/Manpoozle/TrustLaunch/main/trust_launch_template.sh"
+export script_url="https://raw.githubusercontent.com/benjaminbrumbaugh/TrustLaunch/main/simple_tl_script.sh"
+export one_liner_template_url="https://raw.githubusercontent.com/benjaminbrumbaugh/TrustLaunch/main/trust_launch_template.sh"
 
--- update-hash.sh
+++ update-hash.sh
@@ -3,15 +3,14 @@
 # Source configuration variables
 source config.sh
 
-# Fetch the script and compute its hash
+# Fetch the script and compute its hash once
 script_content=$(curl -sS $script_url)
-script_hash=$(curl -sS $script_url | shasum | awk '{print $1}')
+script_hash=$(echo "$script_content" | shasum | awk '{print $1}')
 
 # Fetch the one-liner template
 one_liner_template=$(curl -sS $one_liner_template_url)
 
 # Substituting the placeholders in the template with actual values
-# Assuming placeholders are {{URL}} for the script URL and {{EXPECTED_HASH_PREFIX}} for the hash
 one_liner=$(echo "$one_liner_template" | \
             sed "s|{{URL}}|$script_url|g" | \
             sed "s|{{EXPECTED_HASH_PREFIX}}|$script_hash|g")
